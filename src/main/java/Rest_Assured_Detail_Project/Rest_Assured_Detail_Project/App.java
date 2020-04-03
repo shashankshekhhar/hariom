5e4dd316e059ceb98c978809acf0b8f0478ff74a
Shashankshekhhar <Shashankshekhhar@gmail.com>
2020-04-03 11:55:39 +0530

    First Commit

---
+++ src/main/java/Rest_Assured_Detail_Project/Rest_Assured_Detail_Project/App.java
@@ -0,0 +1,13 @@
+package Rest_Assured_Detail_Project.Rest_Assured_Detail_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Rest_Assured_Detail_Project/Rest_Assured_Detail_Project/testGetREquest.java
@@ -0,0 +1,25 @@
+package Rest_Assured_Detail_Project.Rest_Assured_Detail_Project;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class testGetREquest {
+
+	@Test
+	public void testGetReq() {
+	
+		String id="";
+		RestAssured.baseURI="http://dummy.restapiexample.com/api/v1/employee/1";
+		Response response=null;
+		
+		response=RestAssured.given().when().get();
+		System.out.println("Response :" + response.asString());
+		System.out.println("StatusCOde: "+ response.getStatusCode());
+		
+		assertEquals(200,response.getStatusCode());
+	}
+}
